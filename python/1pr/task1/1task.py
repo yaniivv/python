f1256327d1b55734a5752c16d8876a93ce9ebc2c
yaniivv <yanaa.unicorn@gmail.com>
2022-12-16 12:26:38 +0500

    1task

---
+++ python/1pr/task1/1task.py
@@ -0,0 +1,15 @@
+day = input ('Введите цифру дня недели: ')
+if day == '1':
+    print('Нет')
+if day == '2':
+    print('Нет')
+if day == '3':
+    print('Нет')
+if day == '4':
+    print('Нет')
+if day == '5':
+    print('Нет')
+if day == '6':
+    print('Да')
+if day == '7':
+    print('Да')
